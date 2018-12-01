7205b14ce2a82ccbd6d35caced6ac4575ac6ece9
illmm <gtillmm@gmail.com>
2018-12-01 23:44:04 +0800

    update

--- src/main/java/com/example/practice/thread/Exec.java
+++ src/main/java/com/example/practice/thread/Exec.java
@@ -16,6 +16,7 @@ public class Exec {
          * FixedThreadPool：所有任务只能使用固定大小的线程；
          * SingleThreadExecutor：相当于大小为 1 的 FixedThreadPool。
          */
+
         ExecutorService executorService = Executors.newFixedThreadPool(1);
 
         for (int i = 0; i < 5; i++) {
